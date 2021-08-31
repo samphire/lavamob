26ab00805c0799fabd4f2affc5ac852b0ead579b
samphire <boak67@gmail.com>
2021-08-31 21:36:21 +0900

    Oh boy... STILL getting errors on upload text. Turns out the puncParsedJson AND puncParsedAudioJson required both json_encode AND mysql_real_escape_string. Working now... fingers crossed!

--- php/uploadText.php
+++ php/uploadText.php
@@ -23,9 +23,9 @@ foreach ($data['uniqueWordArray'] as $word) {
     $sql = "SELECT words.id, headwords.id, headword, frequency 
             FROM words JOIN headwords
             ON words.headword_id=headwords.id
-            WHERE word='{$word}'";
+            WHERE word=\"{$word}\"";
 
-    print "\n\n\n\n sql is " . $sql;
+    print "\n\n sql is " . $sql;
 
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_row($result);
@@ -59,27 +59,35 @@ foreach ($data['uniqueWordArray'] as $word) {
 $name = mysqli_real_escape_string($conn, $data['textName']);
 $desc = mysqli_real_escape_string($conn, $data['textDesc']);
 $ppxa = json_encode($data['puncParsedJsonArray'], JSON_UNESCAPED_UNICODE);
-$ppAja = json_encode($data['puncParsedAudioJsonArray'], JSON_UNESCAPED_UNICODE);
+$ppAxa = json_encode($data['puncParsedAudioJsonArray'], JSON_UNESCAPED_UNICODE);
+
+//$ppxa = json_encode($data['puncParsedJsonArray'], JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+//$ppAja = json_encode($data['puncParsedAudioJsonArray'], JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+
+
+
+
 $uia = json_encode($uniqueInfoArray, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 $plainText = mysqli_real_escape_string($conn, $data['plainText']);
 $ppja = mysqli_real_escape_string($conn, $ppxa); //  json encode with no flags...
-echo "plainText:  " . $data['plainText'];
+$ppAja = mysqli_real_escape_string($conn, $ppAxa);
+echo "\n\n\nplainText:  " . $data['plainText'];
 echo "after real escape string:  " . $bob;
 print "\n\n uniqueInfoArray: {$uia}";
 
-echo "name and desc are: " . $name . ", " . $desc;
+echo "\n\nname and desc are: " . $name . ", " . $desc;
 
 $sql = "INSERT INTO `text`(`name`,`description`,`wordcount`,`perc1`,`perc2`,`perc3`,`perc4`,`perc5`,`perc6`,`percREST`,
                    `rarityQuot`,`audio`,`video`,`plainText`,`puncParsedJsonArray`,`audioSpriteJson`,
                    `puncParsedAudioJsonArray`,`uniqueInfoArray`)"
-    . "VALUES ('" . $name . "', '" . $desc . "', " . $data['wordCount']
+    . " VALUES ('" . $name . "', '" . $desc . "', " . $data['wordCount']
     . ", " . $percs[0] . ", " . $percs[1] . ", " . $percs[2] . ", " . $percs[3] . ", " . $percs[4] . ", " . $percs[5]
     . ", " . $percs[6] . ", " . intval($percs[7] / $data['wordCount'] * 100)
     . ", '" . $data['audioFilename'] . "', '" . $data['videoFilename'] . "', \"" . $plainText . "\", '" . $ppja . "', '"
     . $data['audioSpriteObjString'] . "', '" . $ppAja . "', '" . $uia . "')";
 
-echo $sql;
-mysqli_query($conn, $sql) or die("\n" . mysqli_error($conn));
+echo "\n\n$sql";
+mysqli_query($conn, $sql) or die("\n\nERROR IN INSERT QUERY\n\n" . mysqli_error($conn));
 
 echo "\nnew text item added";
 
