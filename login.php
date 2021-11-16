6026833a6caf11966ea87fc03d4702a9f16b92a3
samphire <oakleymatthew@hotmail.com>
2021-11-16 09:22:14 +0900

    changed login.php to optikon rather than panscopic

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 global $conn;
-$conn = mysqli_connect("localhost", "root", "Fk!i=a0@:K", "reader3") or die('Error connecting to mysql');
-$conn2 = mysqli_connect("localhost", "root", "Fk!i=a0@:K", "panscopic") or die('Error connecting to mysql');
+$conn = mysqli_connect("localhost", "root", "Fk!i=a0@:K", "reader3") or die('Error connecting to mysql for conn');
+$conn2 = mysqli_connect("localhost", "root", "Fk!i=a0@:K", "optikon") or die('Error connecting to mysql for conn2');
 mysqli_query($conn, "SET NAMES utf8");
 mysqli_query($conn, "SET CHARACTER SET utf8");
 $sql = "SELECT user_id FROM users WHERE user_email='".$_GET['user_email']."' AND pass_word=md5('".$_GET['pass_word']."')";
@@ -15,8 +15,8 @@ if($numrows == 0){
 }
 list($userid) = mysqli_fetch_row($query);
 
-$sql="SELECT fld_language_id FROM tbl_students where fld_student_id='{$_GET['user_email']}'";
-$languageid=mysqli_query($conn2, $sql)->fetch_object()->fld_language_id;
+$sql="SELECT language_id FROM tbl_students where id='{$_GET['user_email']}'";
+$languageid=mysqli_query($conn2, $sql)->fetch_object()->language_id;
 $arr = Array();
 array_push($arr, $userid);
 array_push($arr, $languageid);
