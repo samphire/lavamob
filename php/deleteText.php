14ece56f7d55bb933a872c35b1dd8481834b80e3
samphire <boak67@gmail.com>
2024-07-08 17:27:40 +0900

    fixed deleteText.php. chatGPT showed me how to make javascript boolean into php boolean.

--- php/deleteText.php
+++ php/deleteText.php
@@ -5,9 +5,11 @@ $userid = $_GET['userid'];
 $textid = $_GET['textid'];
 $isAddToLearned = $_GET['isAddToLearned'];
 
+$isAddToLearned = filter_var($isAddToLearned, FILTER_VALIDATE_BOOLEAN);
+
 echo "isAddToLearned: " . $isAddToLearned;
 
-if ($isAddToLearned == 'true') {
+if ($isAddToLearned) {
     $sql = "SELECT `uniqueInfoArray` FROM `text` WHERE id={$textid}";
     $result = mysqli_query($conn, $sql) or die("query problem in deleteText.php\n\n{$sql}\n\n");
     $words = json_decode(mysqli_fetch_row($result)[0]);
@@ -18,8 +20,7 @@ if ($isAddToLearned == 'true') {
         print_r($item);
         $word = explode("^/", $item);
         $headwordid = $word[2];
-        $sql = "INSERT INTO learned (userid, headwordid)
-        VALUES ({$userid}, {$headwordid})";
+        $sql = "INSERT INTO learned (userid, headwordid) VALUES ({$userid}, {$headwordid})";
         mysqli_query($conn, $sql);
         print("\n");
         echo(mysqli_error($conn));
